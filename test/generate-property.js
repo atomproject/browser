b15f11d2b3fd2f4eaf34765804e1b06f3a4655f5
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-05-26 14:48:15 +0530

    bugs(generate-property & others): Fix following bugs
    * fix the auto generation of object and array type properties
    * hydrolysis doesn't provide string values in objects as we need
    * types provided by hydrolysis are different from basic types
    * rules for the translation of array type
    * fix the bower endpoint validation
    * add testing setup
    * add tests for generate-property module

---
+++ test/generate-property.js
@@ -0,0 +1,62 @@
+let path = require('path');
+let assert = require('chai').assert;
+let Q = require('q');
+Q.longStackSupport = true;
+
+let { createProperty } = require('../lib/generate-property');
+
+it('should generate proper.json file properly', done => {
+  Q.spawn(function* () {
+    let elConfig = {
+      name: 'some-elem',
+      displayName: 'Some Element',
+      filePath: path.join(__dirname, './some-elem.html')
+    };
+
+    let expectedData = {
+      name: elConfig.displayName,
+      properties: [{
+        name: 'Properties',
+        fields: {
+          bool: {
+            name: 'bool',
+            type: 'boolean',
+            value: false
+          },
+
+          str: {
+            name: 'str',
+            type: 'string',
+            value: 'some string'
+          },
+
+          num: {
+            name: 'num',
+            type: 'number',
+            value: 10
+          },
+
+          obj: {
+            name: 'obj',
+            type: 'object'
+          },
+
+          arr: {
+            name: 'arr',
+            type: 'object'
+          },
+
+          dt: {
+            name: 'dt',
+            type: 'object'
+          }
+        }
+      }]
+    };
+
+    let actualData = yield createProperty(elConfig);
+
+    assert.deepEqual(actualData, expectedData);
+    done();
+  });
+});
