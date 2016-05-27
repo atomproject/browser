fb1db2311e888cbc98095b246745ea5779d75ab6
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-05-27 16:33:09 +0530

    test(generate-property & github-url): Fix tests

--- test/generate-property.js
+++ test/generate-property.js
@@ -6,59 +6,86 @@ Q.longStackSupport = true;
 let { createProperty } = require('../lib/generate-property');
 
 describe('property.json generator', () => {
-  it('should generate proper.json file properly', done => {
-    Q.spawn(function* () {
-      let elConfig = {
-        name: 'some-elem',
-        displayName: 'Some Element',
-        filePath: path.join(__dirname, 'some-elem.html')
+  let actualData;
+  let elConfig = {
+    name: 'some-elem',
+    displayName: 'Some Element',
+    filePath: path.join(__dirname, 'some-elem.html')
+  };
+
+  before(() => {
+    return createProperty(elConfig).then(data => actualData = data);
+  });
+
+  it('should have proper basic structure', () => {
+    assert.equal(actualData.name, elConfig.displayName);
+    assert.equal(actualData.properties[0].name, 'Properties');
+  });
+
+  describe('should preserve value and type for', () => {
+    it('boolean property', () => {
+      let fields = actualData.properties[0].fields;
+      let bool = {
+        name: 'bool',
+        type: 'boolean',
+        value: false
       };
 
-      let expectedData = {
-        name: elConfig.displayName,
-        properties: [{
-          name: 'Properties',
-          fields: {
-            bool: {
-              name: 'bool',
-              type: 'boolean',
-              value: false
-            },
+      assert.deepEqual(fields.bool, bool);
+    });
 
-            str: {
-              name: 'str',
-              type: 'string',
-              value: 'some string'
-            },
-
-            num: {
-              name: 'num',
-              type: 'number',
-              value: 10
-            },
-
-            obj: {
-              name: 'obj',
-              type: 'object'
-            },
-
-            arr: {
-              name: 'arr',
-              type: 'object'
-            },
-
-            dt: {
-              name: 'dt',
-              type: 'object'
-            }
-          }
-        }]
+    it('string property', () => {
+      let fields = actualData.properties[0].fields;
+      let str = {
+        name: 'str',
+        type: 'string',
+        value: 'some string'
       };
 
-      let actualData = yield createProperty(elConfig);
+      assert.deepEqual(fields.str, str);
+    });
 
-      assert.deepEqual(actualData, expectedData);
-      done();
+    it('number property', () => {
+      let fields = actualData.properties[0].fields;
+      let num = {
+        name: 'num',
+        type: 'number',
+        value: 10
+      };
+
+      assert.deepEqual(fields.num, num);
+    });
+  });
+
+  it('should remove value for object property', () => {
+    let fields = actualData.properties[0].fields;
+    let obj = {
+      name: 'obj',
+      type: 'object'
+    };
+
+    assert.deepEqual(fields.obj, obj);
+  });
+
+  describe('should remove value and change type to object for', () => {
+    it('array property', () => {
+      let fields = actualData.properties[0].fields;
+      let arr = {
+        name: 'arr',
+        type: 'object'
+      };
+
+      assert.deepEqual(fields.arr, arr);
+    });
+
+    it('any other property', () => {
+      let fields = actualData.properties[0].fields;
+      let dt = {
+        name: 'dt',
+        type: 'object'
+      };
+
+      assert.deepEqual(fields.dt, dt);
     });
   });
 });
--- test/github-url.js
+++ test/github-url.js
@@ -6,14 +6,11 @@ let gurl = require('../lib/github-url');
 
 describe('github url parser', () => {
   it('should handle correct github urls', () => {
-    let ghUrl = 'https://github.com/someUser/someRepo';
-    let expectedOutput = { user: 'someUser', repo: 'someRepo' };
+    let ghUrl = 'https://github.com/xUser/xRepo';
+    let expectedOutput = { user: 'xUser', repo: 'xRepo' };
 
     assert.deepEqual(gurl(ghUrl), expectedOutput);
-
-
-    ghUrl = 'https://github.com/someUser/someRepo/some.txt';
-    assert.deepEqual(gurl(ghUrl), expectedOutput);
+    assert.deepEqual(gurl(ghUrl + '/x/y.txt'), expectedOutput);
   });
 
   it('should handle bad github urls', () => {
