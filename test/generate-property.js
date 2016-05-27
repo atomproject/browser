74690c79b47d798a40d54f1b20e9b46b89672cc1
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-05-27 14:27:10 +0530

    test(generate-property): Fix the test cases

--- test/generate-property.js
+++ test/generate-property.js
@@ -5,58 +5,60 @@ Q.longStackSupport = true;
 
 let { createProperty } = require('../lib/generate-property');
 
-it('should generate proper.json file properly', done => {
-  Q.spawn(function* () {
-    let elConfig = {
-      name: 'some-elem',
-      displayName: 'Some Element',
-      filePath: path.join(__dirname, './some-elem.html')
-    };
+describe('property.json generator', () => {
+  it('should generate proper.json file properly', done => {
+    Q.spawn(function* () {
+      let elConfig = {
+        name: 'some-elem',
+        displayName: 'Some Element',
+        filePath: path.join(__dirname, 'some-elem.html')
+      };
 
-    let expectedData = {
-      name: elConfig.displayName,
-      properties: [{
-        name: 'Properties',
-        fields: {
-          bool: {
-            name: 'bool',
-            type: 'boolean',
-            value: false
-          },
+      let expectedData = {
+        name: elConfig.displayName,
+        properties: [{
+          name: 'Properties',
+          fields: {
+            bool: {
+              name: 'bool',
+              type: 'boolean',
+              value: false
+            },
 
-          str: {
-            name: 'str',
-            type: 'string',
-            value: 'some string'
-          },
+            str: {
+              name: 'str',
+              type: 'string',
+              value: 'some string'
+            },
 
-          num: {
-            name: 'num',
-            type: 'number',
-            value: 10
-          },
+            num: {
+              name: 'num',
+              type: 'number',
+              value: 10
+            },
 
-          obj: {
-            name: 'obj',
-            type: 'object'
-          },
+            obj: {
+              name: 'obj',
+              type: 'object'
+            },
 
-          arr: {
-            name: 'arr',
-            type: 'object'
-          },
+            arr: {
+              name: 'arr',
+              type: 'object'
+            },
 
-          dt: {
-            name: 'dt',
-            type: 'object'
+            dt: {
+              name: 'dt',
+              type: 'object'
+            }
           }
-        }
-      }]
-    };
+        }]
+      };
 
-    let actualData = yield createProperty(elConfig);
+      let actualData = yield createProperty(elConfig);
 
-    assert.deepEqual(actualData, expectedData);
-    done();
+      assert.deepEqual(actualData, expectedData);
+      done();
+    });
   });
 });
